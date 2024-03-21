d39adf033b035edc0bdec57a0be111cd0b472585
Filip Kramar <73394758+FilipKramar@users.noreply.github.com>
2024-03-21 21:54:27 +0100

    Update UserstoryService.java

--- backend/ProjektPraksa/src/main/java/com/atos/projektpraksa/userstory/service/UserstoryService.java
+++ backend/ProjektPraksa/src/main/java/com/atos/projektpraksa/userstory/service/UserstoryService.java
@@ -1,24 +1,20 @@
 package com.atos.projektpraksa.userstory.service;
 
-import com.atos.projektpraksa.task.dto.TaskEditAssigneeDTO;
-import com.atos.projektpraksa.task.dto.TaskListingDTO;
+import com.atos.projektpraksa.task.dto.TaskGetDTO;
 import com.atos.projektpraksa.task.model.Task;
-import com.atos.projektpraksa.user.dto.UserListingDTO;
+import com.atos.projektpraksa.task.service.TaskService;
 import com.atos.projektpraksa.user.model.User;
 import com.atos.projektpraksa.user.repository.UserRepository;
-import com.atos.projektpraksa.userstory.dto.UserstoryCreationDTO;
-import com.atos.projektpraksa.userstory.dto.UserstoryEditAssigneeDTO;
-import com.atos.projektpraksa.userstory.dto.UserstoryEditDTO;
-import com.atos.projektpraksa.userstory.dto.UserstoryListingDTO;
+import com.atos.projektpraksa.userstory.dto.*;
 import com.atos.projektpraksa.userstory.model.Userstory;
 import com.atos.projektpraksa.userstory.repository.UserStoryRepository;
-import com.atos.projektpraksa.usertask.model.UserTask;
 import com.atos.projektpraksa.useruserstory.model.UserUserstory;
 import com.atos.projektpraksa.useruserstory.repository.UserUserstoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serial;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +27,7 @@ public class UserstoryService {
     private final UserStoryRepository userStoryRepository;
     private final UserUserstoryRepository userUserstoryRepository;
     private final UserRepository userRepository;
+    private final TaskService taskService;
     public List<UserstoryListingDTO> getAllUserstories() {
 
         List<Userstory> userstories=userStoryRepository.findAll();
@@ -45,6 +42,7 @@ public class UserstoryService {
                         .name(userstory.getName())
                         .currentStage(userstory.getCurrentStage())
                         .username(userstory.getAssignee().getAssignee().getUsername())
+                        .tasks(userstory.getTasks())
                         .build();
                 listedUserstories.add(userstoryListingDTO);
             }else{
@@ -53,6 +51,7 @@ public class UserstoryService {
                         .name(userstory.getName())
                         .currentStage(userstory.getCurrentStage())
                         .username(null)
+                        .tasks(userstory.getTasks())
                         .build();
                 listedUserstories.add(userstoryListingDTO);
             }
@@ -68,22 +67,24 @@ public class UserstoryService {
     }
     @Transactional
     public Userstory createAnUserstory(UserstoryCreationDTO request) {
-
         Userstory userstory= Userstory.builder()
                 .name(request.getName())
                 .currentStage(request.getCurrentStage())
                 .description(request.getDescription())
                 .complexity(0L)
+                .tasks(taskService.getTasks(request.getTasks()))
                 .build();
 
         userStoryRepository.save(userstory);
+
+        taskService.bindTaskToUserstory(userstory,request.getTasks());
 
 
         return userstory;
 
     }
     @Transactional
-    public Userstory editUserstoryAssignee(UserstoryEditAssigneeDTO request) {
+    public UserUserstory editUserstoryAssignee(UserstoryEditAssigneeDTO request) {
 
 
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
@@ -95,8 +96,13 @@ public class UserstoryService {
 
         Optional<UserUserstory> userUserstoryOptional = userUserstoryRepository.findByUserstoryId(request.getUserstory_id());
         if (userUserstoryOptional.isPresent()) {
-            user.getUserstories().add(userUserstoryOptional.get());
+            UserUserstory userUserstory = userUserstoryOptional.get();
+            userUserstory.setAssignee(user);
+            userUserstory.setLast_edit(timestamp);
+            userUserstoryRepository.save(userUserstory);
+            user.getUserstories().add(userUserstory);
             userRepository.save(user);
+            return  userUserstory;
         } else {
             UserUserstory userUserstory = UserUserstory
                     .builder()
@@ -108,9 +114,8 @@ public class UserstoryService {
             userUserstoryRepository.save(userUserstory);
             user.getUserstories().add(userUserstory);
             userRepository.save(user);
+            return userUserstory;
         }
-
-        return userstory;
     }
 
         public Userstory editUserstory(UserstoryEditDTO request) {
@@ -122,9 +127,44 @@ public class UserstoryService {
         userstory.setName(request.getName());
         userstory.setComplexity(request.getComplexity());
         userstory.setCurrentStage(request.getCurrentStage());
-
+        userstory.setTasks(taskService.getTasks(request.getTasks()));
+        taskService.bindTaskToUserstory(userstory,request.getTasks());
         editUserstoryAssignee(request.getUserstoryEditAssigneeDTO());
         return userstory;
     }
-}
 
+    public UserstoryGetDTO getAnUserstory(Long id) {
+        Userstory userstory = userStoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Userstory not found with ID: " + id));
+
+
+        UserstoryGetDTO userstoryGetDTO;
+        if(userstory.getAssignee()!=null){
+            User user = userRepository.findById(userstory.getAssignee().getAssignee().getId())
+                    .orElseThrow(() -> new RuntimeException("Task not found with ID: " + id));
+            userstoryGetDTO = new UserstoryGetDTO();
+            userstoryGetDTO.setUserstoryId(userstory.getId());
+            userstoryGetDTO.setDescription(userstory.getDescription());
+            userstoryGetDTO.setName(userstory.getName());
+            userstoryGetDTO.setCurrentStage(userstory.getCurrentStage());
+            userstoryGetDTO.setTasks(userstory.getTasks());
+            userstoryGetDTO.setComplexity(userstory.getComplexity());
+            userstoryGetDTO.setUserId(user.getId());
+            userstoryGetDTO.setUsername(user.getUsername());
+        }
+        else{
+            userstoryGetDTO = new UserstoryGetDTO();
+            userstoryGetDTO.setUserstoryId(userstory.getId());
+            userstoryGetDTO.setDescription(userstory.getDescription());
+            userstoryGetDTO.setName(userstory.getName());
+            userstoryGetDTO.setCurrentStage(userstory.getCurrentStage());
+            userstoryGetDTO.setComplexity(userstory.getComplexity());
+            userstoryGetDTO.setTasks(userstory.getTasks());
+            userstoryGetDTO.setUserId(null);
+            userstoryGetDTO.setUsername(null);
+
+        }
+
+        return userstoryGetDTO;
+    }
+}
